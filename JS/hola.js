c464c99a9e9718b771bc42e37c6fa9a91e0a9260
gersonmatos24 <163216743+gersonmatos24@users.noreply.github.com>
2024-10-11 09:22:48 -0500

    Creando Repositorio

---
+++ JS/hola.js
@@ -0,0 +1,34 @@
+// EJERCICIO:
+//  * - Crea un comentario en el código y coloca la URL del sitio web oficial del
+//  *   lenguaje de programación que has seleccionado.
+
+/*https://developer.mozilla.org/es/docs/Web/JavaScript */
+
+// * - Representa las diferentes sintaxis que existen de crear comentarios
+//  *   en el lenguaje (en una línea, varias...).
+
+// Este es un comentario en una sola linea
+/* Este es un comentario 
+en múltiples líneas
+*/
+
+// Crea una variable (y una constante si el lenguaje lo soporta).
+
+// --Creando una variable
+var nombre;
+let cosa;
+
+// -- Creando una constante
+const apellido = "Matos";
+
+// - Crea variables representando todos los tipos de datos primitivos
+//  *   del lenguaje (cadenas de texto, enteros, booleanos...).
+
+const boleean = true; //SOLO ACEPTA VALORES TRUE O FALSE
+const string = "Mi nombre es Gerson"; //SOLO ACEPTA CADENAS DE TEXTOS
+const number = 10; //SOLO ACEPTA NUMEROS ENTEROS O DECIMALES
+const bignumber = 19923345n; //ACEPTA NUMEROS ENTEROS MAS ALLA DEL LIMITE
+
+// * - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+const saludo = "Hola, Java Script";
+console.log(saludo);
---
+++ JS/operadoresYestrucutras.js
@@ -0,0 +1,20 @@
+// * EJERCICIO:
+//  * - Crea ejemplos utilizando todos los tipos de operadores de tu lenguaje:
+//  *   Aritméticos, lógicos, de comparación, asignación, identidad, pertenencia, bits...
+//  *   (Ten en cuenta que cada lenguaje puede poseer unos diferentes)
+
+//Aritmeticos
+const suma = 10 + 3;
+console.log(suma);
+
+const resta = 10 - 3;
+console.log(resta);
+
+const division = 10 / 2;
+console.log(division);
+
+const multiplicacion = 10 * 2;
+console.log(multiplicacion);
+const resto = 10 % 2;
+console.log(resto);
+let a;
